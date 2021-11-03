2bb4343f0429f035024c687739045b92631ad530
Kenneth Kim <kkm2059@gmail.com>
2021-11-03 12:55:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(array, string) {
-    array.filter()
+    return array.filter(function(el) {
+        return el.toLowerCase() == string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(function(el) {
+        return el.startsWith(string)
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function(el) {
+        return el.name == string
+    })
 }
